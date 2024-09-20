36761dc8ae88673883a723a61a933ad504306e92
Syed <rh_syed@outlook.com>
2024-09-19 19:43:18 -0600

    Setup Router

---
+++ app/index.jsx
@@ -0,0 +1,24 @@
+import { Link, Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Aora</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: "blue" }}>
+        Go To Profile
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
